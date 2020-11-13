62083e79feeaa76467ce6156aec2d058f70e417a
Benjamin McFarland <benrmcfarland@gmail.com>
2020-11-13 09:51:26 -0800

    finish test to return age

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,3 +1,4 @@
+import { TestScheduler } from 'jest';
 import { Calculator } from './../src/js/Calculator.js';
 
 describe('Calculator', () => {
@@ -6,6 +7,9 @@ describe('Calculator', () => {
   beforeEach(() => {
     calc = new Calculator(19);
   });
-  
+
+  test('should return the given age', () => {
+    expect(calc.earthAge()).toEqual(19);
+  });
 
 });
