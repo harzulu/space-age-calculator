de1956d3ed539d8134c276fc12d937779093f63f
Benjamin McFarland <benrmcfarland@gmail.com>
2020-11-13 10:45:33 -0800

    Add failing test

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,3 +1,5 @@
+import { parse } from "@babel/core";
+
 export class Calculator {
   constructor(age) {
     this.age = age;
@@ -9,6 +11,11 @@ export class Calculator {
 
   mercuryAge() {
     let num = this.age / 0.24;
+    return parseFloat(num.toFixed(2));
+  }
+
+  venusAge() {
+    let num = this.age / 10.62;
     return parseFloat(num.toFixed(2));
   }
 
