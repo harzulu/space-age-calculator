35c31014bc2d9b7e47ed9eda1c60245cfcee2a85
Benjamin McFarland <benrmcfarland@gmail.com>
2020-11-13 13:30:01 -0800

    Begin UI code, Making results print to page

--- src/main.js
+++ src/main.js
@@ -2,5 +2,15 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { Calculator } from './js/Calculator';
 
-$;
+function printToPage(calc) {
+  console.log(calc.age);
+}
+
+$("#ageForm").submit(function(event) {
+  event.preventDefault();
+  let age = $("#age").val();
+  let calc = new Calculator(age);
+  printToPage(calc);
+});
